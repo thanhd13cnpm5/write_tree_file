18d2a22020f0368605f5942682a47d154481dea7
trandinhthanh <trandinhthanh22102000@gmail.com>
2021-03-04 16:04:12 +0700

    change commit first

---
+++ write_tree/README.txt
@@ -0,0 +1,7 @@
+### CHƯƠNG TRÌNH LIỆT KÊ CẤU TRÚC THƯ MỤC DẠNG TREE
+
+## CÁCH CHẠY CHƯƠNG TRÌNH
+
+# bật terminal nhập dòng lệnh python main.py
+  màn hình in ra yêu cầu nhập url:
+  nhập đường dẫn thư mục mà bạn muốn in ra theo đúng định dạng của url vd:C://root/folder/file
--- write_tree/main.py
+++ write_tree/main.py
@@ -2,10 +2,13 @@ from pathlib import Path
 import ast
 from collections import namedtuple
 
+# prefix
 space = '   '
 branch = '│   '
+# con trỏ
 tee = '├──'
 last = '└── '
+
 Lang = {
     "Javascript": ".js",
     "Css": ".css",
@@ -23,25 +26,36 @@ Lang = {
 LST1 = []
 LST2 = []
 
-def tree_file(Path, prefix = ''):
+
+# hàm in cấu trúc file dạng tree
+def tree_file(Path, prefix=''):
+    # lấy ra danh sách list các đường dẫn
     listpath = list(Path.iterdir())
+    # list các con trỏ ├── ở cuối là └──
     pointers = [tee] * (len(listpath) - 1) + [last]
     for pointer, path in zip(pointers, listpath):
         print(prefix + pointer + path.name)
         if path.suffix == ".py":
             LST2.append(path.as_posix())
+        # lọc tất cả file có trong dect lang{} đã khai báo
         for x in Lang:
             if Lang[x] == path.suffix:
                 LST1.append(path.suffix)
         if path.is_dir():
             extension = branch if pointer == tee else space
+            # đệ quy hàm tree_file
             yield from tree_file(path, prefix=prefix + extension)
 
-for line in tree_file(Path.home() / 'C:\\Users\\DELL\\PycharmProjects\\write_root'):
+
+# cho người dùng nhập vào url
+folder_url = input("-nhập url: ")
+# chạy hàm tree_file và in cấu trúc file dạng tree
+for line in tree_file(Path.home() / folder_url):
     print(line)
 
 print("\n\n")
 dem = 0
+# thống kê file và ngôn ngữ trong file
 for x in Lang:
     for y in LST1:
         if Lang[x] == y:
@@ -55,21 +69,25 @@ print("\n\n")
 
 Import = namedtuple("Import", ["module", "name", "alias"])
 
+
+# hàm kiểm tra module & library được import vào projects
 def get_imports(path):
+    # dọc file
     with open(path) as f:
-       root = ast.parse(f.read(), path)
+        root = ast.parse(f.read(), path)
+    # lặp qua danh sách các node con và kiểm tra
     for node in ast.iter_child_nodes(root):
-        if isinstance(node, ast.Import):
+        if isinstance(node, ast.Import):  # trả về True thì module = []
             module = []
-        elif isinstance(node, ast.ImportFrom):
+        elif isinstance(node, ast.ImportFrom):  # trả về True thì module = node.module.split('.')
             module = node.module.split('.')
         else:
             continue
 
         for n in node.names:
+            # in ra module và library
             yield Import(module, n.name.split('.'), n.asname)
 
 for x in LST2:
     for imp in get_imports(x):
         print(imp)
-
